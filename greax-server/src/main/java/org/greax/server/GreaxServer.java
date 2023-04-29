c32f5af1d7df0993b218d24c84cb0b1c667c910f
luwe <luis.weich.de@gmail.com>
2023-04-30 00:41:38 +0200

    add dropwizard server

--- greax-server/src/main/java/org/greax/server/GreaxServer.java
+++ greax-server/src/main/java/org/greax/server/GreaxServer.java
@@ -1,11 +1,25 @@
 package org.greax.server;
 
+import io.dropwizard.core.Application;
+import io.dropwizard.core.setup.Environment;
 import org.greax.core.Constants;
+import org.greax.core.util.Lazy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class GreaxServer {
+public class GreaxServer extends Application<GreaxServerConfiguration> {
 
-    public static void main(String[] args) {
-        System.out.println("GreaxServer " + Constants.VERSION);
+    private static final Logger log = LoggerFactory.getLogger(GreaxServer.class);
+
+    public static final Lazy<GreaxServer> INSTANCE = Lazy.of(GreaxServer::new);
+
+    public static void main(String[] args) throws Exception {
+        INSTANCE.get().run(args);
+    }
+
+    @Override
+    public void run(GreaxServerConfiguration configuration, Environment environment) {
+        log.info("Version: {}", Constants.VERSION);
     }
 
 }
---
+++ greax-server/src/main/java/org/greax/server/GreaxServerConfiguration.java
@@ -0,0 +1,6 @@
+package org.greax.server;
+
+import io.dropwizard.core.Configuration;
+
+public class GreaxServerConfiguration extends Configuration {
+}
