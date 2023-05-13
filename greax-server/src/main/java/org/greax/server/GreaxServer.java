f731cede89d0f605bd37d3f0eebfddd1df1c9044
luwe <luis.weich.de@gmail.com>
2023-05-13 16:10:31 +0200

    update

--- greax-server/src/main/java/org/greax/server/GreaxServer.java
+++ greax-server/src/main/java/org/greax/server/GreaxServer.java
@@ -11,10 +11,10 @@ public class GreaxServer extends Application<GreaxServerConfiguration> {
 
     private static final Logger log = LoggerFactory.getLogger(GreaxServer.class);
 
-    public static final Lazy<GreaxServer> INSTANCE = Lazy.of(GreaxServer::new);
+    private static final Lazy<GreaxServer> instance = Lazy.of(GreaxServer::new);
 
     public static void main(String[] args) throws Exception {
-        INSTANCE.get().run(args);
+        instance.get().run(args);
     }
 
     @Override
