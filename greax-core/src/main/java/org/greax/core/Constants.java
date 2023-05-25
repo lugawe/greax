d4ea8246af42015987de038d87d109cc682f9875
luwe <luis.weich.de@gmail.com>
2023-05-25 23:55:05 +0200

    constants package names

--- greax-core/src/main/java/org/greax/core/Constants.java
+++ greax-core/src/main/java/org/greax/core/Constants.java
@@ -4,6 +4,12 @@ public final class Constants {
 
     public static final String VERSION = "1.0-SNAPSHOT";
 
+    public static final String CORE_PACKAGE = "org.greax.core";
+
+    public static final String CLIENT_PACKAGE = "org.greax.client";
+
+    public static final String SERVER_PACKAGE = "org.greax.server";
+
     private Constants() {
     }
 
